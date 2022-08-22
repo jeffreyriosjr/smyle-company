e5accd09e9319edf51b2a78910046ce2663ca48d
jeffrey rios jr <jeffriosjr@gmail.com>
2022-08-22 00:15:24 -0700

    templates set up

--- header.php
+++ header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	<script src="https://kit.fontawesome.com/ba414641d6.js" crossorigin="anonymous"></script>
 
 	<?php wp_head(); ?>
 </head>
