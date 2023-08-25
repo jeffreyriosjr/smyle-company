b72d39b44a74fc37e3dc491942177f5fbbfdd97d
jeffrey rios jr <jeffriosjr@gmail.com>
2023-08-24 18:59:45 -0700

    new page added

---
+++ podcast-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * <?php
+
+ *  * Template Name: Podcast Page
+ 
+ *
+ * @package smyle_co_
+ */
+get_header();
+
+?>
+<main id="primary" class="site-main">
+<?php
+		while ( have_posts() ) :
+			the_post();
+
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+   
+    <?php the_content(); ?> 
+</main>
+
+
+<?php
+get_footer();
